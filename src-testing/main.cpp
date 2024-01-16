ed55c67954c344bd32fd8b1a5d64753a97b0d815
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-01-16 05:08:59 -0300

    add mult_conj

--- src-testing/main.cpp
+++ src-testing/main.cpp
@@ -10,7 +10,9 @@
  * Don't judge the code you might see in there! :)
  **********************************************************************/
 
+#include "common/dsp/buffer.h"
 #include "logger.h"
+#include <cstdint>
 #include <fstream>
 #include <cstring>
 #include <volk/volk.h>
@@ -28,14 +30,36 @@ int main(int /*argc*/, char *argv[])
     std::ifstream data_in(argv[1], std::ios::binary);
     std::ofstream data_out(argv[2], std::ios::binary);
 
+    size_t max_buffer_size;
+    max_buffer_size = dsp::STREAM_BUFFER_SIZE;
+
+
     complex_t input_buffer[BUFFER_SIZE];
     complex_t output_buffer[BUFFER_SIZE];
+
+    complex_t delayed_output[BUFFER_SIZE];
+    complex_t not_delayed_output[BUFFER_SIZE];
+
+
+    complex_t last_samp = 0;
+
 
     while (!data_in.eof())
     {
 	    data_in.read((char *)input_buffer, BUFFER_SIZE * sizeof(complex_t));
 
-	    volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+	    for (int i = 0; i < BUFFER_SIZE; i++)
+	    {
+		    delayed_output[i] = i == 0 ? last_samp : input_buffer[i - 1];
+		    not_delayed_output[i] = input_buffer[i];
+	    }
+
+	    last_samp = input_buffer[BUFFER_SIZE - 1];
+
+	    volk_32fc_x2_multiply_conjugate_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)not_delayed_output, (lv_32fc_t *)delayed_output, BUFFER_SIZE);
+
+
+	    //volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
 
 	    data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
     }
