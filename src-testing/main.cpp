0066e6f0a34477ea67e3b2e6f703dd8d4ecbbf80
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-01-16 06:50:29 -0300

    add exponentiate

--- src-testing/main.cpp
+++ src-testing/main.cpp
@@ -10,19 +10,23 @@
  * Don't judge the code you might see in there! :)
  **********************************************************************/
 
+#include "common/cli_utils.h"
 #include "common/dsp/buffer.h"
 #include "logger.h"
 #include <cstdint>
 #include <fstream>
 #include <cstring>
+#include <string>
 #include <volk/volk.h>
 #include <volk/volk_complex.h>
 #include "common/image/image.h"
 #include "common/dsp/complex.h"
+#include "nlohmann/json.hpp"
 
 #define BUFFER_SIZE 8192
 
-int main(int /*argc*/, char *argv[])
+
+int main(int argc, char *argv[])
 {
     initLogger();
     completeLoggerInit();
@@ -48,18 +52,49 @@ int main(int /*argc*/, char *argv[])
     {
 	    data_in.read((char *)input_buffer, BUFFER_SIZE * sizeof(complex_t));
 
-	    for (int i = 0; i < BUFFER_SIZE; i++)
+	    nlohmann::json parameters = parse_common_flags(argc - 3, &argv[3]);
+
+	    //if (std::string(argv[3]) == "multiply_conjugate")
+	    if (parameters.contains("multiply_conjugate"))
 	    {
-		    delayed_output[i] = i == 0 ? last_samp : input_buffer[i - 1];
-		    not_delayed_output[i] = input_buffer[i];
+		    logger->info("Multiply conjugate selected!");
+
+		    // Delay 1 sample
+		    for (int i = 0; i < BUFFER_SIZE; i++)
+	    	    {
+	    	            delayed_output[i] = i == 0 ? last_samp : input_buffer[i - 1];
+	    	            not_delayed_output[i] = input_buffer[i];
+	    	    }
+            	    
+	    	    last_samp = input_buffer[BUFFER_SIZE - 1];
+            	    
+		    // Multiply conjugate
+	    	    volk_32fc_x2_multiply_conjugate_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)not_delayed_output, (lv_32fc_t *)delayed_output, BUFFER_SIZE);
 	    }
+	    //data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
 
-	    last_samp = input_buffer[BUFFER_SIZE - 1];
+	    //else if (std::string(argv[3]) == "exponential")
+	    if (parameters.contains("exponentiate"))
+	    {
+		    uint8_t exponent;
 
-	    volk_32fc_x2_multiply_conjugate_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)not_delayed_output, (lv_32fc_t *)delayed_output, BUFFER_SIZE);
-
-
-	    //volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+		    try {
+		    exponent = parameters["exponentiate"].get<uint8_t>();
+		    }
+		    catch (std::exception &e)
+		    {
+			    logger->error("Error parsing argument! %s", e.what());
+		    }
+		    if (exponent > 1) {
+			    volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+			    for (int i = 2; i < exponent; i++) {
+				    volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+			    }
+		    }
+		    else {
+		    logger->error("Exponent number must be > 1!!");
+		    }
+	    }
 
 	    data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
     }
