413e7edc9a625a18ad3234680efa9bb2b4cc436b
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-03-02 22:43:13 -0300

    fix testing

--- src-testing/main.cpp
+++ src-testing/main.cpp
@@ -15,7 +15,6 @@
 #include "common/dsp/filter/fir.h"
 #include "common/dsp/filter/firdes.h"
 #include "logger.h"
-<<<<<<< HEAD
 #include <complex>
 #include <cstdint>
 #include <fstream>
@@ -24,31 +23,20 @@
 #include <string>
 #include <volk/volk.h>
 #include <volk/volk_complex.h>
-=======
-#include "common/projection/projs2/proj.h"
 
->>>>>>> master
 #include "common/image/image.h"
 #include "common/dsp/complex.h"
 #include "nlohmann/json.hpp"
 #include "common/dsp/block.h"
 
-<<<<<<< HEAD
 #define BUFFER_SIZE 8192
 
-=======
-#include "common/map/map_drawer.h"
-
-#include "common/image/image_meta.h"
-#include "common/projection/projs2/proj_json.h"
->>>>>>> master
 
 int main(int argc, char *argv[])
 {
     initLogger();
     completeLoggerInit();
 
-<<<<<<< HEAD
     std::ifstream data_in(argv[1], std::ios::binary);
     std::ofstream data_out(argv[2], std::ios::binary);
 
@@ -91,46 +79,8 @@ int main(int argc, char *argv[])
     {
 	    logger->info("Low pass filter selected!");
     }
-=======
-#if 0
-    proj::projection_t p;
-
-    p.type = proj::ProjType_Geos;
-    //   p.proj_offset_x = 418962.397137703;
-    //   p.proj_offset_y = -101148.834767705;
-    //   p.proj_scalar_x = 60.5849500687633;  // 1;
-    //   p.proj_scalar_y = -60.5849500687633; // 1;
-    //   p.lam0 = 2 * DEG2RAD;
-    //   p.phi0 = 48 * DEG2RAD;
-    p.params.altitude = 300000;
->>>>>>> master
-
-    proj::projection_setup(&p);
-
-    double x = 10000; // 15101; // 678108.04;
-    double y = 10000; // 16495; // 5496954.89;
-    double lon, lat;
-    proj::projection_perform_inv(&p, x, y, &lon, &lat);
-
-    logger->info("X %f - Y %f", x, y);
-    logger->info("Lon %f - Lat %f", lon, lat);
-
-    proj::projection_perform_fwd(&p, lon, lat, &x, &y);
-
-    logger->info("X %f - Y %f", x, y);
-#else
-
-    image::Image<uint16_t> image_test;
-    image_test.load_tiff(argv[1]);
-    image_test.equalize();
-    image_test.normalize();
-    // image_test.mirror(false, true);
-
-    logger->info("PROC DONE");
-
-    if (!image::has_metadata(image_test))
+    while (!data_in.eof())
     {
-<<<<<<< HEAD
 	    data_in.read((char *)input_buffer, BUFFER_SIZE * sizeof(complex_t));
 
 
@@ -185,52 +135,3 @@ int main(int argc, char *argv[])
 	    data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
     }
 }
-=======
-        logger->error("Meta error!");
-        return 0;
-    }
-
-    auto jsonp = image::get_metadata(image_test);
-    logger->debug("\n%s", jsonp.dump(4).c_str());
-    proj::projection_t p = jsonp["proj_cfg"];
-    bool v = proj::projection_setup(&p);
-
-    if (v)
-    {
-        logger->error("Proj error!");
-        return 0;
-    }
-
-    {
-        double x = 0; // 15101; // 678108.04;
-        double y = 0; // 16495; // 5496954.89;
-        double lon, lat;
-        proj::projection_perform_inv(&p, x, y, &lon, &lat);
-
-        logger->info("X %f - Y %f", x, y);
-        logger->info("Lon %f - Lat %f", lon, lat);
-
-        proj::projection_perform_fwd(&p, lon, lat, &x, &y);
-
-        logger->info("X %f - Y %f", x, y);
-    }
-
-    {
-        unsigned short color[4] = {0, 65535, 0, 65535};
-        map::drawProjectedMapShapefile({"resources/maps/ne_10m_admin_0_countries.shp"},
-                                       image_test,
-                                       color,
-                                       [&p](double lat, double lon, int, int) -> std::pair<int, int>
-                                       {
-                                           double x, y;
-                                           proj::projection_perform_fwd(&p, lon, lat, &x, &y);
-                                           return {(int)x, (int)y};
-                                       });
-    }
-
-    image_test.save_tiff(argv[2]);
-
-    proj::projection_free(&p);
-#endif
-}
->>>>>>> master
