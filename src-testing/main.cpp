9762ba6939d0c523799114d379c0aec16e55ef8c
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-03-02 22:37:22 -0300

    add testing

--- src-testing/main.cpp
+++ src-testing/main.cpp
@@ -15,23 +15,83 @@
 #include "common/dsp/filter/fir.h"
 #include "common/dsp/filter/firdes.h"
 #include "logger.h"
+<<<<<<< HEAD
+#include <complex>
+#include <cstdint>
+#include <fstream>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <volk/volk.h>
+#include <volk/volk_complex.h>
+=======
 #include "common/projection/projs2/proj.h"
 
+>>>>>>> master
 #include "common/image/image.h"
 #include "common/dsp/complex.h"
 #include "nlohmann/json.hpp"
 #include "common/dsp/block.h"
 
+<<<<<<< HEAD
+#define BUFFER_SIZE 8192
+
+=======
 #include "common/map/map_drawer.h"
 
 #include "common/image/image_meta.h"
 #include "common/projection/projs2/proj_json.h"
+>>>>>>> master
 
 int main(int argc, char *argv[])
 {
     initLogger();
     completeLoggerInit();
 
+<<<<<<< HEAD
+    std::ifstream data_in(argv[1], std::ios::binary);
+    std::ofstream data_out(argv[2], std::ios::binary);
+
+    size_t max_buffer_size;
+    max_buffer_size = dsp::STREAM_BUFFER_SIZE;
+
+
+    complex_t input_buffer[BUFFER_SIZE];
+    complex_t output_buffer[BUFFER_SIZE];
+
+    complex_t delayed_output[BUFFER_SIZE];
+    complex_t not_delayed_output[BUFFER_SIZE];
+
+
+    complex_t last_samp = 0;
+
+    nlohmann::json parameters = parse_common_flags(argc - 3, &argv[3]);
+
+    if (parameters.contains("multiply_conjugate"))
+    {
+	    logger->info("Multiply conjugate selected!");
+    }
+    if (parameters.contains("exponentiate"))
+    {
+	    logger->info("Exponentiate selected!");
+	    uint8_t exponent;
+	    try {
+		    exponent = parameters["exponentiate"].get<uint8_t>();
+	    }
+	    catch (std::exception &e)
+	    {
+		    logger->error("Error parsing argument! %s", e.what());
+	    }
+	    if (exponent <= 1)
+	    {
+		    logger->error("Exponent number must be > 1!!");
+	    }
+    }
+    if (parameters.contains("LPF"))
+    {
+	    logger->info("Low pass filter selected!");
+    }
+=======
 #if 0
     proj::projection_t p;
 
@@ -43,6 +103,7 @@ int main(int argc, char *argv[])
     //   p.lam0 = 2 * DEG2RAD;
     //   p.phi0 = 48 * DEG2RAD;
     p.params.altitude = 300000;
+>>>>>>> master
 
     proj::projection_setup(&p);
 
@@ -69,6 +130,62 @@ int main(int argc, char *argv[])
 
     if (!image::has_metadata(image_test))
     {
+<<<<<<< HEAD
+	    data_in.read((char *)input_buffer, BUFFER_SIZE * sizeof(complex_t));
+
+
+	    //if (std::string(argv[3]) == "multiply_conjugate")
+	    if (parameters.contains("multiply_conjugate"))
+	    {
+
+		    // Delay 1 sample
+		    for (int i = 0; i < BUFFER_SIZE; i++)
+	    	    {
+	    	            delayed_output[i] = i == 0 ? last_samp : input_buffer[i - 1];
+	    	            not_delayed_output[i] = input_buffer[i];
+	    	    }
+            	    
+	    	    last_samp = input_buffer[BUFFER_SIZE - 1];
+            	    
+		    // Multiply conjugate
+	    	    volk_32fc_x2_multiply_conjugate_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)not_delayed_output, (lv_32fc_t *)delayed_output, BUFFER_SIZE);
+	    }
+	    //data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
+
+	    //else if (std::string(argv[3]) == "exponential")
+	    if (parameters.contains("exponentiate"))
+	    {
+		    uint8_t exponent;
+
+		    try {
+		    exponent = parameters["exponentiate"].get<uint8_t>();
+		    }
+		    catch (std::exception &e)
+		    {
+			    logger->error("Error parsing argument! %s", e.what());
+		    }
+			    volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+			    for (int i = 2; i < exponent; i++) {
+				    volk_32fc_x2_multiply_32fc((lv_32fc_t *)output_buffer, (lv_32fc_t *)output_buffer, (lv_32fc_t *)input_buffer, BUFFER_SIZE);
+			    }
+	    }
+
+	    if (parameters.contains("LPF"))
+	    {
+//		    std::shared_ptr<dsp::FIRBlock<complex_t>> lpf;
+//		    //std::shared_ptr<dsp::FIRBlock<complex_t>> lpf;
+//		    //lpf = std::make_shared<dsp::FIRBlock<complex_t>>(input_buffer, dsp::firdes::low_pass(1.0, 8000000, 200000, 2000));
+//		    //lpf = std::make_shared<dsp::FIRBlock<std::complex_t>>(output_stream, dsp::firdes::low_pass(1.0, 8000000, 200000, 2000));
+//
+//		    lpf = std::make_shared<dsp::FIRBlock<complex_t>>(output_buffer, dsp::firdes::low_pass(1.0, 8000000, 50000, 100));
+//		    lpf->input_stream->read();
+		    
+	    }
+
+	    data_out.write((char *)output_buffer, BUFFER_SIZE * sizeof(complex_t));
+    }
+}
+=======
         logger->error("Meta error!");
         return 0;
     }
@@ -116,3 +233,4 @@ int main(int argc, char *argv[])
     proj::projection_free(&p);
 #endif
 }
+>>>>>>> master
